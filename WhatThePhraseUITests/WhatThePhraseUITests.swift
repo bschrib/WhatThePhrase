c063e7f2022625ede59602eeff91b3168b21a613
Brandon Schreiber <2302045+bschrib@users.noreply.github.com>
2023-12-27 16:19:19 -0500

    moar

--- WhatThePhraseUITests/WhatThePhraseUITests.swift
+++ WhatThePhraseUITests/WhatThePhraseUITests.swift
@@ -1,33 +1,35 @@
-//
-//  WhatThePhraseUITests.swift
-//  WhatThePhraseUITests
-//
-//  Created by Brandon Schreiber on 4/19/23.
-//
-
 import XCTest
 
 final class WhatThePhraseUITests: XCTestCase {
     let app = XCUIApplication()
     
     func testTakeScreenshots() async throws {
-      await setupSnapshot(app)
-      app.launch()
+        await setupSnapshot(app)
+
+        DispatchQueue.main.async {
+            self.app.launch()
+        }
         await snapshot("01CategoryView")
         
-      app.scrollViews.otherElements.buttons["Places & Spaces"].tap()
-      app.buttons["Start"].tap()
+        DispatchQueue.main.async {
+            self.app.scrollViews.otherElements.buttons["Places & Spaces"].tap()
+            self.app.buttons["Start"].tap()
+        }
         await snapshot("02GameView")
         
-      app.buttons["Team 2"].tap()
-      let stopButton = app.buttons["Stop"]
-      stopButton.tap()
+        DispatchQueue.main.async {
+            self.app.buttons["Team 2"].tap()
+            let stopButton = self.app.buttons["Stop"]
+            stopButton.tap()
+        }
         await snapshot("03GameViewComplete")
         
-      let okButton = app.alerts["Time's Up!"].scrollViews.otherElements.buttons["OK"]
-      okButton.tap()
-      app.buttons["Go Back To Categories"].tap()
-      app.navigationBars["Select Category"]/*@START_MENU_TOKEN@*/.buttons["gearshape"]/*[[".otherElements[\"gearshape\"].buttons[\"gearshape\"]",".buttons[\"gearshape\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        DispatchQueue.main.async {
+            let okButton = self.app.alerts["Time's Up!"].scrollViews.otherElements.buttons["OK"]
+            okButton.tap()
+            self.app.buttons["Go Back To Categories"].tap()
+            self.app.navigationBars["Select Category"].buttons["gearshape"].tap()
+        }
         await snapshot("04SettingsView")
     }
 }
